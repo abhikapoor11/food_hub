4986f5dd76f5284c50fa80ce4f20d8414bf05f44
abhikapoor11 <75206295+abhikapoor11@users.noreply.github.com>
2021-05-19 16:52:19 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'food.apps.FoodConfig',
+    'rest_framework',
     'crispy_forms',
 ]
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
